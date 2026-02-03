0a621c8787941aed85119b2a9081d4eef53cfcba
Fernandes Lucas <lucas.fernandes3319@gmail.com>
2026-02-03 10:00:18 +0100

    Fin TP9 Début bonus

--- TP1/src/App.jsx
+++ TP1/src/App.jsx
@@ -8,13 +8,16 @@ import { useState, useEffect } from 'react';
 function App() {
   const [pokedex, setPokedex] = useState([]);
   const [pokemondata, setDataPokemon] = useState([]);
+  const [loading, setLoading] = useState (false); 
+
 
   useEffect(() => {
     fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/1")
       .then((response) => response.json())
       .then((pokemondata) => {
         console.log(pokemondata);
-        setDataPokemon(pokemondata);  
+        setDataPokemon(pokemondata);
+        setLoading(true);  
       })
       .catch((err) => {
         console.log(err.message);
@@ -33,12 +36,18 @@ function App() {
       <h1>
         <img src={logo} alt="logo" />
       </h1>
+      {
+        loading ? (
       <div className="lmj-layout-inner">
         <Sidebar pokedex={pokedex} />
         <main>{helloList}</main>
       </div>
+        ) : (
+          <p style={{textAlign: "center", width: "100%", color: "white"}}>Chargement des données...</p>
+        )
+      }
     </>
   );
-};
+}
 
 export default App;
